1296643e7264532d492e6c471992bdc9e6880f1a
Shivam Raj <74100226+srvcode@users.noreply.github.com>
2022-03-30 14:34:07 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " .PHP_VERSION. "\n";
-print "A change is in the air!!\n"
-print "New Change shivam\n"
+print "A change is in the air!!\n";
+print "New Change shivam\n";
 ?>
